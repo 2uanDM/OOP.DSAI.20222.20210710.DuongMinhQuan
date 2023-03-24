3bb51b7876299d335cd9c47ec25f081c5daa5125
2uanDM <hokage321xxx@gmail.com>
2023-03-24 22:33:43 +0700

    finished

--- HelloWorld/src/FirstCode.java
+++ HelloWorld/src/FirstCode.java
@@ -1,5 +1,7 @@
+import javax.swing.plaf.basic.BasicBorders.MarginBorder;
+
 public class FirstCode {
-	public static void main(String[] args) {
-		System.out.println("hello world");
+	public static void main (String[] args) {
+		System.out.println("Hello");
 	}
 }
