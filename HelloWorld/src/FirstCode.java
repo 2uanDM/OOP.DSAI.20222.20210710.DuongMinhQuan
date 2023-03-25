e34e3e8b26a7de3fd892ced441c75e53836aaa84
2uanDM <hokage321xxx@gmail.com>
2023-03-25 09:32:10 +0700

    Update FirstCode.java

--- HelloWorld/src/FirstCode.java
+++ HelloWorld/src/FirstCode.java
@@ -1,7 +1,12 @@
-import javax.swing.plaf.basic.BasicBorders.MarginBorder;
-
 public class FirstCode {
 	public static void main (String[] args) {
-		System.out.println("Hello");
+		int age = 15; 
+		long bignum = 4_132_343_242L;
+		char key = 'A'; 
+		float num = 4.3121F;
+		System.out.println(age);
+		System.out.println(bignum % age);
+		System.out.println(key);
+		System.out.println(num * age);
 	}
 }
