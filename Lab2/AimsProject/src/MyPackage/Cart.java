a041b2eea373264401d9f9e93cca78d77dfa3e59
2uanDM <hokage321xxx@gmail.com>
2023-05-16 23:47:44 +0700

    lab2 done

--- Lab2/AimsProject/src/MyPackage/Cart.java
+++ Lab2/AimsProject/src/MyPackage/Cart.java
@@ -2,9 +2,13 @@ package MyPackage;
 
 public class Cart {
     public static final int MAX_NUMBERS_ORDERED = 20;
-    private DigitalVideoDisc itemsOrdered[] = 
-        new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
-    private int qtyOrdered = 0;
+    private DigitalVideoDisc[] itemsOrdered;
+    private int qtyOrdered;
+
+    public Cart() {
+        itemsOrdered = new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
+        qtyOrdered = 0;
+    }
 
     public void addDigitalVideoDisc(DigitalVideoDisc disc) {
         if (qtyOrdered == MAX_NUMBERS_ORDERED) {
@@ -20,7 +24,7 @@ public class Cart {
         for (int i = 0; i < qtyOrdered; i++) {
             if (itemsOrdered[i].isEqual(disc)) {
                 // Shift the remaining items to fill the gap
-                for (int j = i; j < qtyOrdered - 1; ++j) {
+                for (int j = i; j < qtyOrdered - 1; j++) {
                     itemsOrdered[j] = itemsOrdered[j+1];
                 } 
                 itemsOrdered[qtyOrdered-1] = null; // Remove the last reference
@@ -37,7 +41,20 @@ public class Cart {
         for (int i = 0; i < qtyOrdered; i++) {
             total += itemsOrdered[i].getCost();
         }
-        return total;
+        return Math.round(total * 100.0) / 100.0;
+    }
+
+    public void getItemsOrdered() {
+        for (int i = 0; i < qtyOrdered; i++) {
+            DigitalVideoDisc disc = itemsOrdered[i];
+            System.out.println("-------------------------------");
+            System.out.println("Disc " + (i+1) + ":");
+            System.out.println("Title: " + disc.getTitle()); 
+            System.out.println("Category: " + disc.getCategory()); 
+            System.out.println("Director: " + disc.getDirector()); 
+            System.out.println("Cost: " + disc.getCost()); 
+            System.out.println("Length: " + disc.getLength()); 
+        }
     }
 
 }
