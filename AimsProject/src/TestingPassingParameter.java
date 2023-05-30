be081e0c6c24d237bdbd3da89c82b4f74aaf7741
2uanDM <hokage321xxx@gmail.com>
2023-05-30 23:29:03 +0700

    Update TestingPassingParameter.java

--- AimsProject/src/TestingPassingParameter.java
+++ AimsProject/src/TestingPassingParameter.java
@@ -3,24 +3,29 @@ public class TestingPassingParameter {
         // TODO Auto-generated method stub
         DigitalVideoDisc jungleDVD = new DigitalVideoDisc("Jungle");
         DigitalVideoDisc cinderellaDVD = new DigitalVideoDisc("Cinderella");
-
-        swap(jungleDVD, cinderellaDVD);
+    
+        Object[] dvdArray = {jungleDVD, cinderellaDVD};
+        swap(dvdArray);
+    
+        jungleDVD = (DigitalVideoDisc) dvdArray[0];
+        cinderellaDVD = (DigitalVideoDisc) dvdArray[1];
+    
         System.out.println("jungle DVD title: " + jungleDVD.getTitle());
         System.out.println("cinderella DVD title: " + cinderellaDVD.getTitle());
-
+    
         changeTitle(jungleDVD, cinderellaDVD.getTitle());
         System.out.println("jungle dvd title: " + jungleDVD.getTitle());
     }
 
-    public static void swap(Object s1, Object s2) {
-        Object tmp = s1; 
-        s1 = s2; 
-        s2 = tmp;
+    public static void swap(Object[] array) {
+        Object tmp = array[0];
+        array[0] = array[1];
+        array[1] = tmp;
     }
 
     public static void changeTitle(DigitalVideoDisc dvd, String title) { 
         String oldTitle = dvd.getTitle();
         dvd.setTitle((title));
-        dvd = new DigitalVideoDisc(oldTitle);
+        // dvd = new DigitalVideoDisc(oldTitle);
     }
 }
