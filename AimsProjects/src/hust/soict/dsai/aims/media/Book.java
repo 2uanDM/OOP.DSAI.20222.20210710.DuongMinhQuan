126b220d146ddb5a288be000ebcf8d804444b3b9
2uanDM <hokage321xxx@gmail.com>
2023-06-19 11:18:37 +0700

    create Book.java

---
+++ AimsProjects/src/hust/soict/dsai/aims/media/Book.java
@@ -0,0 +1,49 @@
+package hust.soict.dsai.aims.media;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Book extends Media {
+    private List<String> authors = new ArrayList<String>();
+
+    // Constructor
+    public Book(int id, String title, String category, float cost) {
+        this.setId(id);
+        this.setTitle(title);
+        this.setCategory(category);
+        this.setCost(cost);
+    }
+
+    public Book(String title) {
+        this.setTitle(title);
+    }
+
+    // toString()
+    public String toString() {
+        return String.format("Class: %s, Title: %s, Category: %s, Cost: %:.2f",
+                this.getClass(), this.getTitle(), this.getCategory(), this.getCost());
+    }
+
+    public boolean addAuthor(String author) {
+        for (String name : authors) {
+            if (author.equals(name)) {
+                System.out.println(author + " author is already in the list");
+                return false;
+            }
+        }
+
+        authors.add(author);
+        return true;
+    }
+
+    public boolean removeAuthor(String author) {
+        for (String name : authors) {
+            if (author.equals(name)) {
+                authors.remove(name);
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
