235131d6bad8b6bd52e980582c72528f90c04f3b
2uanDM <hokage321xxx@gmail.com>
2023-06-25 22:04:25 +0700

    add Exception class

--- AimsProjects/src/hust/soict/dsai/aims/screen/MediaStore.java
+++ AimsProjects/src/hust/soict/dsai/aims/screen/MediaStore.java
@@ -1,93 +1,154 @@
 package hust.soict.dsai.aims.screen;
 
-import java.awt.*;
+import hust.soict.dsai.aims.media.*;
+import hust.soict.dsai.aims.cart.Cart;
+import hust.soict.dsai.aims.exception.*;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.*;
-
-import hust.soict.dsai.aims.cart.Cart;
-import hust.soict.dsai.aims.media.Media;
-import hust.soict.dsai.aims.media.Playable;
-import hust.soict.dsai.aims.store.Store;
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 public class MediaStore extends JPanel {
+	private Media media;
+	private Cart cart;
 
-    private Media media;
+	public MediaStore(Media media, Cart cart) {
+		this.media = media;
+		this.cart = cart;
+		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
-    public MediaStore(Media media) {
-        this.media = media;
-        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		JLabel title = new JLabel(media.getTitle());
+		title.setFont(new Font(title.getFont().getName(), Font.PLAIN, 20));
+		title.setAlignmentX(CENTER_ALIGNMENT);
 
-        JLabel title = new JLabel(media.getTitle());
-        title.setFont(new Font(title.getFont().getName(), Font.PLAIN, 20));
-        title.setAlignmentX(CENTER_ALIGNMENT);
+		JLabel cost = new JLabel("" + media.getCost() + " $");
+		cost.setAlignmentX(CENTER_ALIGNMENT);
 
-        JLabel cost = new JLabel("" + media.getCost() + " $");
-        cost.setAlignmentX(CENTER_ALIGNMENT);
+		JPanel container = new JPanel();
+		container.setLayout(new FlowLayout(FlowLayout.CENTER));
 
-        JPanel container = new JPanel();
-        container.setLayout(new FlowLayout(FlowLayout.CENTER));
+		JButton addToCartButton = new JButton("Add to cart");
+		addToCartButton.addActionListener(new AddToCartListener());
+		container.add(addToCartButton);
 
-        // Add to cart button
-        JButton addToCartButton = new JButton("Add to cart");
-        addToCartButton.addActionListener(new addToCartListener());
-        container.add(addToCartButton);
+		JButton detailsButton = new JButton("View details");
+		detailsButton.addActionListener(new DetailsListener());
+		container.add(detailsButton);
 
-        // Play button
-        if (media instanceof Playable) {
-            JButton playButton = new JButton("Play");
-            playButton.addActionListener(new playButtonListener());
-            container.add(playButton);
-        }
+		if (media instanceof Playable) {
+			JButton playButton = new JButton("Play");
+			playButton.addActionListener(new PlayButtonListener());
+			container.add(playButton);
+		}
 
-        this.add(Box.createVerticalGlue());
-        this.add(title);
-        this.add(cost);
-        this.add(Box.createVerticalGlue());
-        this.add(container);
+		this.add(Box.createVerticalGlue());
+		this.add(title);
+		this.add(cost);
+		this.add(Box.createVerticalGlue());
+		this.add(container);
 
-        this.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-    }
+		this.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+	}
 
-    private class playButtonListener implements ActionListener {
-        private final JDialog playing;
+	private class PlayButtonListener implements ActionListener {
 
-        public playButtonListener() {
-            playing = new JDialog();
-            playing.setLayout(new BorderLayout());
-            playing.setSize(200, 100);
-            JLabel label = new JLabel("");
-            label.setHorizontalAlignment(JLabel.CENTER);
-            playing.add(label, BorderLayout.CENTER);
-        }
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			try {
+				((Disc) media).play();
+			} catch (PlayerException ex) {
+				JPanel p = new JPanel();
+				JDialog d = new JDialog();
+				JLabel j1 = new JLabel(media.getTitle() + " cannot be played");
+				JLabel j2 = new JLabel("Media length is non-positive");
+				p.setLayout(new BoxLayout(p, BoxLayout.Y_AXIS));
+				j1.setAlignmentX(Component.CENTER_ALIGNMENT);
+				j2.setAlignmentX(Component.CENTER_ALIGNMENT);
+				p.add(Box.createVerticalGlue());
+				p.add(j1);
+				p.add(j2);
+				p.add(Box.createVerticalGlue());
+				d.add(p);
+				d.setSize(250, 100);
+				Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+				int w = d.getSize().width;
+				int h = d.getSize().height;
+				int x = (dim.width - w) / 2;
+				int y = (dim.height - h) / 2;
+				d.setLocation(x, y);
+				d.setVisible(true);
+			}
+		}
+	}
 
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            JLabel label = (JLabel) playing.getContentPane().getComponent(0);
-            label.setText("Playing: " + media.getTitle());
-            playing.setVisible(true);
-        }
-    }
+	private class AddToCartListener implements ActionListener {
 
-    private class addToCartListener implements ActionListener {
-        private final JDialog adding;
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			JPanel p = new JPanel();
+			JDialog d = new JDialog();
+			JLabel l = new JLabel();
+			try {
+				cart.addMedia(media);
+				l.setText(media.getTitle() + " added to cart");
+			} catch (CartFullException ex) {
+				l.setText("The cart is full");
+			} finally {
+				p.setLayout(new BoxLayout(p, BoxLayout.Y_AXIS));
+				l.setAlignmentX(Component.CENTER_ALIGNMENT);
+				p.add(Box.createVerticalGlue());
+				p.add(l);
+				p.add(Box.createVerticalGlue());
+				d.add(p);
+				d.setSize(200, 100);
+				Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+				int w = d.getSize().width;
+				int h = d.getSize().height;
+				int x = (dim.width - w) / 2;
+				int y = (dim.height - h) / 2;
+				d.setLocation(x, y);
+				d.setVisible(true);
+			}
+		}
+	}
 
-        public addToCartListener() {
-            adding = new JDialog();
-            adding.setLayout(new BorderLayout());
-            adding.setSize(200, 100);
-            JLabel label = new JLabel("");
-            label.setHorizontalAlignment(JLabel.CENTER);
-            adding.add(label, BorderLayout.CENTER);
-        }
+	private class DetailsListener implements ActionListener {
 
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            JLabel label = (JLabel) adding.getContentPane().getComponent(0);
-            label.setText("Added " + adding.getTitle());
-            adding.setVisible(true);
-        }
-    }
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			JPanel p = new JPanel();
+			JDialog d = new JDialog();
+			JLabel l = new JLabel("<html>" + media.getDetails().replaceAll("\n", "<br/>") + "</html>",
+					SwingConstants.CENTER);
+			p.setLayout(new BoxLayout(p, BoxLayout.Y_AXIS));
+			l.setAlignmentX(Component.CENTER_ALIGNMENT);
+			p.add(Box.createVerticalGlue());
+			p.add(l);
+			p.add(Box.createVerticalGlue());
+			d.add(p);
+			d.setSize(200, 200);
+			Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+			int w = d.getSize().width;
+			int h = d.getSize().height;
+			int x = (dim.width - w) / 2;
+			int y = (dim.height - h) / 2;
+			d.setLocation(x, y);
+			d.setVisible(true);
+		}
 
+	}
 }
