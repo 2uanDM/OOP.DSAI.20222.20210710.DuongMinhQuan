c0084c3b35cbf644d3744def84cfa0a4176626f6
2uanDM <hokage321xxx@gmail.com>
2023-06-25 22:05:16 +0700

    done for lab 05

--- AimsProjects/src/hust/soict/dsai/aims/media/MediaComparatorByTitleCost.java
+++ AimsProjects/src/hust/soict/dsai/aims/media/MediaComparatorByTitleCost.java
@@ -1,17 +1,20 @@
 package hust.soict.dsai.aims.media;
-
 import java.util.Comparator;
 
 public class MediaComparatorByTitleCost implements Comparator<Media> {
-    @Override
-    public int compare(Media m1, Media m2) {
-        int c = m1.getTitle().compareTo(m2.getTitle());
-        if (c == 0) {
-            if (m1.getCost() < m2.getCost())
-                c = -1;
-            else if (m1.getCost() > m2.getCost())
-                c = 1;
-        }
-        return c;
-    }
+
+	public int compare(Media m1, Media m2) {
+		int titleComparison = m1.getTitle().compareToIgnoreCase(m2.getTitle());
+		if (titleComparison != 0) {
+			return titleComparison;
+		} else {
+			if (m1.getCost() < m2.getCost()) {
+				return 1;
+			} else if (m1.getCost() > m2.getCost()) {
+				return -1;
+			} else {
+				return 0;
+			}
+		}
+	}
 }
--- AimsProjects/src/hust/soict/dsai/aims/screen/StoreScreen.java
+++ AimsProjects/src/hust/soict/dsai/aims/screen/StoreScreen.java
@@ -1,124 +1,212 @@
 package hust.soict.dsai.aims.screen;
 
-import java.awt.*;
+import hust.soict.dsai.aims.store.*;
+import hust.soict.dsai.aims.cart.*;
+import hust.soict.dsai.aims.exception.*;
+import hust.soict.dsai.aims.media.*;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
-import java.awt.event.*;
 
-import javax.swing.*;
-
-import hust.soict.dsai.aims.store.Store;
-import hust.soict.dsai.aims.cart.Cart;
-import hust.soict.dsai.aims.media.Book;
-import hust.soict.dsai.aims.media.CompactDisc;
-import hust.soict.dsai.aims.media.DigitalVideoDisc;
-import hust.soict.dsai.aims.media.Media;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
 
 public class StoreScreen extends JFrame {
-    private Store store;
+	private Store store;
+	private Cart cart;
 
-    public StoreScreen(Store store) {
-        this.store = store;
-        Container cp = getContentPane();
-        cp.setLayout(new BorderLayout());
+	public static void main(String[] args) throws Exception {
+		//Test
+		Store myStore = new Store();
+		Cart myCart = new Cart();
 
-        cp.add(createNorth(), BorderLayout.NORTH);
-        cp.add(createCenter(), BorderLayout.CENTER);
+		//Create DVDs
+		DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
+		DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
+		DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
+		Track track1 = new Track("Bruh1", 13);
+		Track track2 = new Track("Bruh2", 21);
+		Track track3 = new Track("Bruh3");
+		CompactDisc cd1 = new CompactDisc("Allstars", "Yes", "Smash Mouth", "Dunno", 69.420f);
+		cd1.addTrack(track1);
+		cd1.addTrack(track2);
+		CompactDisc cd2 = new CompactDisc("Allstars 2", "Yes", "Smash Mouth", "Dunno", 69.420f);
+		cd2.addTrack(track1);
+		cd2.addTrack(track3);
+		cd2.addTrack(track2);
+		Book book1 = new Book("Sherlock Holmes", "Detective", 420, 5f);
+		book1.addAuthor("Conan Doyle");
+		DigitalVideoDisc dvd4 = new DigitalVideoDisc("Star Wars 2", "Science Fiction", "George Lucas", 87, 24.95f);
+		DigitalVideoDisc dvd5 = new DigitalVideoDisc("Star Wars 3", "Science Fiction", "George Lucas", 87, 24.95f);
+		DigitalVideoDisc dvd6 = new DigitalVideoDisc("Star Wars 4", "Science Fiction", "George Lucas", 87, 24.95f);
+		DigitalVideoDisc dvd7 = new DigitalVideoDisc("Star Wars 5", "Science Fiction", "George Lucas", 87, 24.95f);
+		DigitalVideoDisc dvd8 = new DigitalVideoDisc("Star Wars 6", "Science Fiction", "George Lucas", 87, 24.95f);
+		DigitalVideoDisc dvd9 = new DigitalVideoDisc("Star Wars 7", "Science Fiction", "George Lucas", 87, 24.95f);
+		DigitalVideoDisc dvd10 = new DigitalVideoDisc("Aladin2", "Animation", 18.99f);
 
-        setVisible(true);
-        setTitle("Store");
-        setSize(1024, 768);
-    }
+		//Try adding DVDs
+//		myStore.addMedia(dvd2, 5);
+		myStore.addMedia(dvd2);
+		myStore.addMedia(dvd1);
+		myStore.addMedia(dvd3);
+		myStore.addMedia(cd1);
+		myStore.addMedia(cd2);
+		myStore.addMedia(book1);
+		myStore.addMedia(dvd4);
+		myStore.addMedia(dvd5);
+		myStore.addMedia(dvd6);
+		myStore.addMedia(dvd7);
+		myStore.addMedia(dvd8);
+		myStore.addMedia(dvd9);
+		myStore.addMedia(dvd10);
+		new StoreScreen(myStore, myCart);
+	}
+	
+	public StoreScreen(Store store, Cart cart) {
+		this.store = store;
+		this.cart = cart;
+		Container cp = getContentPane();
+		cp.setLayout(new BorderLayout());
+		
+		cp.add(createNorth(), BorderLayout.NORTH);
+		cp.add(createCenter(), BorderLayout.CENTER);
+		
+		setVisible(true);
+		setTitle("Store");
+		setSize(1024,768);
+		
+		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+		int w = getSize().width;
+        int h = getSize().height;
+        int x = (dim.width - w) / 2;
+        int y = (dim.height - h) / 2;
+        setLocation(x,y);
+	}
+	
+	JPanel createNorth() {
+		JPanel north = new JPanel();
+		north.setLayout(new BoxLayout(north, BoxLayout.Y_AXIS));
+		north.add(createMenuBar());
+		north.add(createHeader());
+		return north;
+	}
+	
+	JPanel createCenter() {
+		JPanel center = new JPanel();
+		center.setLayout(new GridLayout(5,5,2,2));
+		
+		ArrayList<Media> mediaInStore = store.getItemsInStore();
+		for (Media media: mediaInStore) {
+			MediaStore cell = new MediaStore(media, cart);
+			center.add(cell);
+		}
+		
+		return center;
+	}
+	
+	JMenuBar createMenuBar() {
+		JMenu menu = new JMenu("Options");
+		
+		JMenu smUpdateStore = new JMenu("Update Store");
+		JMenuItem addBook = new JMenuItem("Add Book");
+		addBook.addActionListener(new AddBookListener());
+		smUpdateStore.add(addBook);
+		JMenuItem addCD = new JMenuItem("Add CD");
+		addCD.addActionListener(new AddCDListener());
+		smUpdateStore.add(addCD);
+		JMenuItem addDVD = new JMenuItem("Add DVD");
+		addDVD.addActionListener(new AddDVDListener());
+		smUpdateStore.add(addDVD);
+		
+		menu.add(smUpdateStore);
+		menu.add(new JMenuItem("View store"));
+		JMenuItem cart = new JMenuItem("View cart");
+		cart.addActionListener(new ViewCartListener());
+		menu.add(cart);
+		
+		JMenuBar menuBar = new JMenuBar();
+		menuBar.setLayout(new FlowLayout(FlowLayout.LEFT));
+		menuBar.add(menu);
+		
+		return menuBar;
+	}
+	
+	JPanel createHeader() {
+		JPanel header = new JPanel();
+		header.setLayout(new BoxLayout(header, BoxLayout.X_AXIS));
+		
+		JLabel title = new JLabel("AIMS");
+		title.setFont(new Font(title.getFont().getName(), Font.PLAIN, 50));
+		title.setForeground(Color.CYAN);
+		
+		JButton cart = new JButton("View cart");
+		cart.setPreferredSize(new Dimension(100,50));
+		cart.setMaximumSize(new Dimension(100,50));
+		cart.addActionListener(new ViewCartListener());
+		
+		header.add(Box.createRigidArea(new Dimension(10,10)));
+		header.add(title);
+		header.add(Box.createHorizontalGlue());
+		header.add(cart);
+		header.add(Box.createRigidArea(new Dimension(10,10)));
+		
+		return header;
+	}
+	
+	private class ViewCartListener implements ActionListener {
 
-    JPanel createNorth() {
-        JPanel north = new JPanel();
-        north.setLayout(new BoxLayout(north, BoxLayout.Y_AXIS));
-        north.add(createMenuBar());
-        north.add(createHeader());
-        return north;
-    }
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			new CartScreen(store, cart);
+			dispose();
+		}
+		
+	}
+	
+	private class AddDVDListener implements ActionListener {
 
-    JPanel createHeader() {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			new AddDVDToStoreScreen(store, cart);
+			dispose();
+		}
+		
+	}
+	
+	private class AddBookListener implements ActionListener {
 
-        JPanel header = new JPanel();
-        header.setLayout(new BoxLayout(header, BoxLayout.X_AXIS));
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			new AddBookToStoreScreen(store, cart);
+			dispose();
+		}
+		
+	}
+	
+	private class AddCDListener implements ActionListener {
 
-        JLabel title = new JLabel("AIMS");
-        title.setFont(new Font(title.getFont().getName(), Font.PLAIN, 50));
-        title.setForeground(Color.CYAN);
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			new AddCDToStoreScreen(store, cart);
+			dispose();
+		}
+		
+	}
 
-        JButton cart = new JButton("View cart");
-        cart.setPreferredSize(new Dimension(100, 50));
-        cart.setMaximumSize(new Dimension(100, 50));
-
-        header.add(Box.createRigidArea(new Dimension(10, 10)));
-        header.add(title);
-
-        header.add(Box.createHorizontalGlue());
-
-        header.add(cart);
-
-        header.add(Box.createRigidArea(new Dimension(10, 10)));
-
-        return header;
-    }
-
-    JMenuBar createMenuBar() {
-        JMenu menu = new JMenu("Options");
-
-        JMenu smUpdateStore = new JMenu("Update Store");
-        smUpdateStore.add(new JMenuItem("Add Book"));
-        smUpdateStore.add(new JMenuItem("Add CD"));
-        smUpdateStore.add(new JMenuItem("Add DVD"));
-
-        menu.add(smUpdateStore);
-        menu.add(new JMenuItem("View store"));
-        menu.add(new JMenuItem("View cart"));
-
-        JMenuBar menuBar = new JMenuBar();
-        menuBar.setLayout(new FlowLayout(FlowLayout.LEFT));
-        menuBar.add(menu);
-
-        return menuBar;
-    }
-
-    JPanel createCenter() {
-        JPanel center = new JPanel();
-        center.setLayout(new GridLayout(3, 3, 2, 2));
-
-        ArrayList<Media> mediaInStore = store.getItemsInStores();
-        for (int i = 0; i < mediaInStore.size(); i++) {
-            MediaStore cell = new MediaStore(mediaInStore.get(i));
-            center.add(cell);
-        }
-
-        return center;
-    }
-
-    public static void main(String[] args) {
-        Store store = new Store();
-
-        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Shawshank Redemption", "Drama", "Frank Darabont", 142, 8.75f);
-        DigitalVideoDisc dvd2 = new DigitalVideoDisc("The Godfather", "Drama", "Francis Ford Coppola", 175, 9.2f);
-        DigitalVideoDisc dvd3 = new DigitalVideoDisc("The Dark Knight", "Action", "Christopher Nolan", 152, 9.0f);
-
-        Book book1 = new Book(250, "To Kill a Mockingbird", "Fiction", 12.5f);
-        Book book2 = new Book(180, "1984", "Fiction", 10f);
-        Book book3 = new Book(320, "Sapiens: A Brief History of Humankind", "Non-Fiction", 15.25f);
-
-        CompactDisc cd1 = new CompactDisc("Thriller", "Pop", 42.3f, "Michael Jackson", 9);
-        CompactDisc cd2 = new CompactDisc("Back in Black", "Rock", 36.7f, "AC/DC", 10);
-        CompactDisc cd3 = new CompactDisc("The Eminem Show", "Hip-Hop", 64.4f, "Eminem", 15);
-
-        store.addMedia(dvd1);
-        store.addMedia(dvd2);
-        store.addMedia(dvd3);
-        store.addMedia(book1);
-        store.addMedia(book2);
-        store.addMedia(book3);
-        store.addMedia(cd1);
-        store.addMedia(cd2);
-        store.addMedia(cd3);
-
-        new StoreScreen(store);
-    }
 }
